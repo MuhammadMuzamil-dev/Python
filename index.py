b01985acb70ec53e5eb64168e410477fe6d1fb44
MuhammadMuzamil-dev <zynovodigital@gmail.com>
2026-08-08 18:11:44 +0500

    Initial Python File and Code and its Guide for Installation and VS Code Extensions for Python Running and GUI Interfaces

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-nnz
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Python GUI Window")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Hello World!", font=("Arial", 16))
+label.pack(pady=50)
+
+root.mainloop()
